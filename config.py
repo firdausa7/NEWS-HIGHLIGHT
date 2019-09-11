96282bcb19e3a3ae80b2ddc8ca06cdabbdd8574b
firdausa7 <firdausa.salat@gmail.com>
2019-09-11 21:26:28 +0300

    installed and imported dontenv

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config:
     '''
     General configuration parent class
     '''
+    SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    EVERYTHING_SOURCE_BASE_URL = 'https://newsapi.org/v2/everythingNews?sources={}&pageSize={}&apiKey={}'
+    HEADLINES_BASE_URL = 'https://newsapi.org/v2/headlines?language=en&pageSize={}&apiKey={}'
+    EVERYTHINGNEWS_BASE_URL = 'https://newsapi.org/v2/everythingNews?sources=bbc-news,al-jazeera-english,cnn,independent,google-news,the-telegraph,mashable,the-lad-bible,buzzfeed,bloomberg,engadget,espn,fortune&sortBy=publishedAt&pageSize={}&apiKey={}'
+    SEARCH_API_BASE_URL = 'https://newsapi.org/v2/everythingNews?language=en&q={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
@@ -28,3 +35,8 @@ class DevConfig(Config):
 
     DEBUG = True
 
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
